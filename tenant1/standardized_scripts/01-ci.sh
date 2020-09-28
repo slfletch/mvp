a1eb670ccf04d12e3a14511c25426068450a975b
Ubuntu <ubuntu@stacey-0.localdomain>
2020-09-28 20:55:48 +0000

    UPdating scripts

--- tenant1/standardized_scripts/01-ci.sh
+++ tenant1/standardized_scripts/01-ci.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
-helm init --client-only
 
-#Build rabbitmq chart
+#Create temporary directory
 build_dir=$(mktemp -d)
 cd $build_dir
 git clone https://github.com/openstack/openstack-helm-infra.git
@@ -19,7 +18,6 @@ helm lint ./rabbitmq-0.1.0.tgz
 kubectl get -n harbor secrets harbor-harbor-harbor-nginx -o 'go-template={{ index .data "ca.crt" | base64decode }}' > ./temp-ca.crt
 
 #Adding the chart repository to helm
-helm repo add --ca-file ./temp-ca.crt --username=admin --password=Harbor12345 tcicd https://stacey-0.localdomain:30003/chartrepo/tcicd
-#Push the chart to repository
+
 helm plugin install https://github.com/chartmuseum/helm-push
-helm push --ca-file ./temp-ca.crt --username=admin --password=Harbor12345 ./rabbitmq-0.1.0.tgz tcicd
+helm push --ca-file harbor-ca.crt --username=admin --password=Harbor12345 ./test/rabbitmq-0.1.0.tgz tcicd
--- tenant1/standardized_scripts/03-test.sh
+++ tenant1/standardized_scripts/03-test.sh
@@ -1,4 +1,3 @@
 # run helm test
 set -ex
-helm init --client-only
 helm test demo-rabbitmq --logs --cleanup --timeout 600
