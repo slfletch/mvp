285c401ac59de0af0755310a30131d68e7416368
Stacey Fletcher <staceylynnfletcher@gmail.com>
2020-11-30 13:04:23 -0600

    Updating cert

--- tenant1/standardized_scripts/04-publish.sh
+++ tenant1/standardized_scripts/04-publish.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 set -ex
 kubectl get -n harbor secrets harbor-release-harbor-nginx -o 'go-template={{ index .data "ca.crt" | base64decode }}' > ./temp-ca.crt
-
+tail -f /dev/null
 #Adding the chart repository to helm
 helm push --ca-file temp-ca.crt --username=admin --password=Harbor12345 ./rabbitmq-0.1.2.tgz nrf
