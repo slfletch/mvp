b495c64d37e494d64fd7d9632923d1fc54c8736a
Stacey Fletcher <staceylynnfletcher@gmail.com>
2020-11-29 23:54:32 -0600

    updating script

--- tenant1/standardized_scripts/01-ci.sh
+++ tenant1/standardized_scripts/01-ci.sh
@@ -18,6 +18,4 @@ helm lint ./rabbitmq-0.1.2.tgz
 kubectl get -n harbor secrets harbor-release-harbor-nginx -o 'go-template={{ index .data "ca.crt" | base64decode }}' > ./temp-ca.crt
 
 #Adding the chart repository to helm
-
-helm plugin install https://github.com/chartmuseum/helm-push
 helm push --ca-file harbor-ca.crt --username=admin --password=Harbor12345 ./test/rabbitmq-0.1.0.tgz nrf
